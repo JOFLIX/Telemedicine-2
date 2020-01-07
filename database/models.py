e26e943cdd089260af2201cd5e6e90a7d6de478e
AleksandraOrzechowska <aorzechowska_999@wp.pl>
2020-01-07 13:21:53 +0100

    Database

--- database/models.py
+++ database/models.py
@@ -1,5 +1,25 @@
 from django.db import models
 
-class Doctor(models.Model):
+class Doctors(models.Model):
+	doctor_id = models.AutoField(primary_key=True)
 	first_name = models.CharField(max_length=50)
 	last_surname = models.CharField(max_length=50)
+
+class Patients(models.Model):
+	pesel = models.AutoField(primary_key=True)
+	first_name = models.CharField(max_length=50)
+	last_surname = models.CharField(max_length=50)
+
+class Conferences(models.Model):
+	conference_id = models.AutoField(primary_key=True)
+	pesel = models.ForeignKey(Patients, on_delete=models.CASCADE)
+	doctor_id = models.ForeignKey(Doctors, on_delete=models.PROTECT)
+
+class TestResults(models.Model):
+	result_id = models.AutoField(primary_key=True)
+	pesel = models.ForeignKey(Patients, on_delete=models.CASCADE)
+	doctor_id = models.ForeignKey(Doctors, on_delete=models.PROTECT)
+	result_text = models.TextField()
+	result_image = models.BinaryField()
+	comment_image = models.BinaryField()
+	comment_text = models.TextField()
